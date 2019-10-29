0d4536e260ead4358a076ef3080ccfd4049c2795
Sebastian Delima <sebastiandelima@Cactus1.local>
2019-10-29 16:26:41 -0400

    with_movies

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -13,8 +13,7 @@ def get_character_movies_from_api(character_name)
     end
   end
 
-    # puts movies
-    # binding.pry
+   
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
